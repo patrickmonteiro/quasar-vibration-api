08aa88cd21ade6fd7b0023f28892fef2778d3249
patryckx <patrick.monteiro@w3as.com>
2019-07-19 11:47:45 -0300

    First Commit :tada:

---
+++ src/pages/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page class="row full-height items-center">
+    <div class="col text-center">
+      <div class="text-h6 text-negative" v-if="!enableVibration">Your browser does not support the Vibration API</div>
+      <q-btn :color="enableVibration ? 'purple' : 'grey-6'" icon="vibration" label="Test Vibration"
+        class="full-width" size="lg" @click="vibration()" :disable="!enableVibration" />
+    </div>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      enableVibration: false
+    }
+  },
+  mounted () {
+    if (window.navigator.vibrate) {
+      this.enableVibration = true
+    }
+  },
+  methods: {
+    vibration () {
+      window.navigator.vibrate([200, 100, 200])
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
